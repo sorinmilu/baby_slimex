a054072ffc1958d4b87ec8590e0a135d1484f779
root <root@WLEROBUH1-00603.asnet.accorservices.net>
2024-08-23 02:07:06 +0300

    log separator

--- index.php
+++ index.php
@@ -9,19 +9,38 @@ use Psr\Log\LoggerInterface;
 use Slim\Factory\AppFactory;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 use DI\Container;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
+
 // Create Container using PHP-DI
 $container = new Container();
 
-// Set up logging
+// Create a custom log format with pipe separators
+$logFormat = "%datetime% | %level_name% | %message%\n";
+
+// Create a LineFormatter with the custom format
+$formatter = new LineFormatter($logFormat, null, true, true);
+
+// Create a logger instance
 $log = new Logger('slim_app');
+
+
 $logLevel = $config['log_level'] ?? Logger::DEBUG;
-$log->pushHandler(new StreamHandler($config['log_file'], $logLevel));
+
+// Create a StreamHandler to write logs to a file
+$streamhandler = new StreamHandler($config['log_file'], $logLevel);
+
+// Assign the custom formatter to the StreamHandler
+$streamhandler->setFormatter($formatter);
+
+// Set up logging
+$log->pushHandler($streamhandler);
+
 
 // Add logger to container
 $container->set(LoggerInterface::class, function() use ($log) {
@@ -41,17 +60,20 @@ $app = AppFactory::create();
 $app->add(TwigMiddleware::createFromContainer($app));
 $app->getContainer()->get('view')->getEnvironment()->addGlobal('asset_path', $config['asset_path']);
 // Function to fetch and save a random background image from Picsum
+
+
 function fetchBackgroundImage($size, $tmpDir, LoggerInterface $log) {
     $client = new Client(['allow_redirects' => true]);
     $imageUrl = "https://picsum.photos/{$size}";
 
     if (!is_dir($tmpDir)) {
-        $log->debug("Creating target directory for images: ". $tmpDir);
+        $log->debug("| | Creating target directory for images: ". $tmpDir);
         mkdir($tmpDir, 0755, true);
     }
 
     try {
-        $log->debug("Fetching image from URL: $imageUrl");
+
+        $log->debug("| | Fetching image from URL: $imageUrl");
 
         // Send a GET request to the image URL
         $response = $client->request('GET', $imageUrl, ['stream' => true]);
@@ -60,7 +82,7 @@ function fetchBackgroundImage($size, $tmpDir, LoggerInterface $log) {
         $redirectHistory = $response->getHeader('X-Guzzle-Redirect-History');
         $finalUrl = end($redirectHistory) ?: $imageUrl;
 
-        $log->debug("Final URL after redirection: $finalUrl");
+        $log->debug("| | Final URL after redirection: $finalUrl");
 
         // Get the image content from the final URL
         $imageResponse = $client->request('GET', $finalUrl, ['stream' => true]);
@@ -73,7 +95,7 @@ function fetchBackgroundImage($size, $tmpDir, LoggerInterface $log) {
         // Save the image content to the file
         file_put_contents($filePath, $imageContent);
 
-        $log->debug("Image saved to: $filePath");
+        $log->debug("| | Image saved to: $filePath");
 
         return $filename; // Return the filename to be used in the URL
 
@@ -86,11 +108,12 @@ function fetchBackgroundImage($size, $tmpDir, LoggerInterface $log) {
 // Function to get or fetch a background image
 function getBackgroundImage($config, LoggerInterface $log) {
     $cachedImages = glob($config['tmp_dir'] . 'bg_*.jpg');
-    $log->debug("we have ". count($cachedImages) . " images in cache");
+
+    $log->debug("| | we have ". count($cachedImages) . " images in cache");
     
     // Use cache if there are more than 10 images
     if (count($cachedImages) > $config['cached_images'] -1) {
-        $log->debug("Selecting a random cached background image.");
+        $log->debug("| | Selecting a random cached background image.");
         $randomImage = $cachedImages[array_rand($cachedImages)];
         return basename($randomImage);
     }
@@ -115,7 +138,7 @@ $app->get('/', function ($request, $response, $args) use ($container,$config) {
 $app->get('/joke', function ($request, $response, $args) use ($container, $config) {
     $log = $container->get(LoggerInterface::class);
     $ip = $request->getServerParams()['REMOTE_ADDR'] ?? 'unknown';
-    $log->debug($ip . ': Joke route accessed');
+    $log->info($ip . '|  Joke route accessed');
 
     $backgroundImageFilename = getBackgroundImage($config, $log);
     $backgroundImageUrl = $backgroundImageFilename ? '/tmp/' . $backgroundImageFilename : '';
@@ -125,7 +148,7 @@ $app->get('/joke', function ($request, $response, $args) use ($container, $confi
         $jokeApiResponse = $client->request('GET', 'https://official-joke-api.appspot.com/random_joke');
 	$joke = json_decode($jokeApiResponse->getBody());
 
-        $log->debug('Joke retrieved: ' . $joke->setup);
+        $log->debug($ip . ' | Joke retrieved: ' . $joke->setup);
     } catch (RequestException $e) {
         $log->error("Error getting joke: " . $e->getMessage());
         return $this->get('view')->render($response, 'err.twig', []);
@@ -143,7 +166,7 @@ $app->get('/cocktail', function ($request, $response, $args) use ($container, $c
     $log = $container->get(LoggerInterface::class);
     $ip = $request->getServerParams()['REMOTE_ADDR'] ?? 'unknown';
 
-    $log->debug($ip . ': Cocktail route accessed');
+    $log->info($ip. ' |  Cocktail route accessed');
 
     $backgroundImageFilename = getBackgroundImage($config, $log);
     $backgroundImageUrl = $backgroundImageFilename ? '/tmp/' . $backgroundImageFilename : '';
@@ -154,7 +177,7 @@ $app->get('/cocktail', function ($request, $response, $args) use ($container, $c
         $cocktailApiResponse = $client->request('GET', 'https://www.thecocktaildb.com/api/json/v1/1/random.php');
         $cocktail = json_decode($cocktailApiResponse->getBody())->drinks[0];
     } catch (RequestException $e) {
-        $log->error("Error getting recipe: " . $e->getMessage());
+        $log->error($this->get('clientIp'). "| $this->get('clientIp'). "| "Error getting recipe: " . $e->getMessage());
         return $this->get('view')->render($response, 'err.twig', []);
     }
     
