2586aa057a8a990d65e73f2e42d51834d05ce1fd
sorinmilu <sorinmilu@gmail.com>
2024-10-15 21:06:36 +0300

    nginx config for azure deployment

--- startup.sh
+++ startup.sh
@@ -4,7 +4,8 @@ MARKER_FILE=/home/site/wwwroot/.init_script_ran
 if [ ! -f "$MARKER_FILE" ]; then
     echo "Running initial setup script..."
     # Place your one-time setup tasks here
-
+    cp -f /home/site/wwwroot/nginx-config/default /etc/nginx/sites-enabled/default
+    service nginx reload
     # Create the marker file after successful execution
     touch "$MARKER_FILE"
     echo "Initial setup completed."
