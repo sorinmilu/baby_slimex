1117022a1157ececa1d81c046d6f0ea01ddd2495
sorinmilu <sorinmilu@gmail.com>
2024-10-15 23:15:47 +0300

    more stuff in the startup

--- startup.sh
+++ startup.sh
@@ -12,6 +12,25 @@ if [ ! -f "$MARKER_FILE" ]; then
     log "Running initial setup script..."
     # Place your one-time setup tasks here
 
+    log "Installing jq"
+    apt install -y jq
+
+    log "Call the RandomUser API and extract the first name, last name, and year of birth"
+
+    output_file=/home/site/wwwroot/assets/name.txt
+    log "name file is: $output_file"
+
+    response=$(curl -s "https://randomuser.me/api/?results=1&nat=us,gb,ca,au,nz" | jq -r '.results[0] | "\(.name.first) \(.name.last), \(.dob.date | split("T")[0] | split("-")[0])"')
+
+    if [ -n "$response" ]; then
+        log "got response $response, writing to file $output_file"
+        echo "$response" > "$output_file"
+    else
+        log "no response, writing john doe"
+        echo "John Doe, 1900" > "$output_file"
+    fi
+
+
     # Create the marker file after successful execution
     touch "$MARKER_FILE"
     log "Initial setup completed."
